496b0f4c8b80f2be7a46fdcae3a36de6cc411810
srirammookkandi-prog <sriram.mookkandi@gmail.com>
2026-09-17 06:38:34 +0530

    fix: shimmer updated

--- src/components/Shimmer/ResShimmerCard.jsx
+++ src/components/Shimmer/ResShimmerCard.jsx
@@ -2,8 +2,8 @@ const ResShimmerCard = () => {
   return (
     <div className=' mx-5 my-2 bg-gray-300'>
       <div className='flex justify justify-between'>
-        <div className="w-[800px] h-[200px] m-5 rounded-4xl bg-gray-200 " ></div>
-        <div className="w-[150px] h-[150px] bg-gray-200 mx-5 my-auto"></div>
+        <div className="w-200 h-50 m-5 rounded-4xl bg-gray-200 " ></div>
+        <div className="w-37.5 h-37.5 bg-gray-200 mx-5 my-auto"></div>
       </div>
     </div>
   )
--- src/components/Shimmer/RestaurantShimmer.jsx
+++ src/components/Shimmer/RestaurantShimmer.jsx
@@ -5,12 +5,12 @@ const RestaurantShimmer = () => {
   return (
     <div>
       <div className=" flex filter m-1 justify-center px-10">
-        <div className="w-[300px] h-[40px]  bg-gray-300 mx-1 my-2"></div>
+        <div className="w-75 h-10  bg-gray-300 mx-1 my-2"></div>
       </div>
       <div className="flex filter m-1 justify-center px-10">
-        <div className="w-[500px] h-[30px]  bg-gray-300 mx-5 my-2"></div>
+        <div className="w-125 h-7.5  bg-gray-300 mx-5 my-2"></div>
       </div>
-      <h1 className='w-[300px] h-[30px] bg-gray-300 mx-62 '></h1>
+      <h1 className='w-75 h-7.5 bg-gray-300 mx-62 '></h1>
       <div className="flex flex-wrap justify-around my-1">
         <ResShimmerCard />
         <ResShimmerCard />
